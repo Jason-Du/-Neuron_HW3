67044b4c3cceb1c953951be0d46eb4cf56fd74b5
“Jason-Duh(Home) <dushiun@gmail.com>
2020-11-02 13:33:36 +0800

    1102

---
+++ adult_train.py
@@ -0,0 +1,236 @@
+from keras.layers import Layer
+from keras import backend as K
+from keras.utils import np_utils
+import tensorflow as tf
+from tensorflow import keras
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import PairwiseKernel
+import pandas as pd
+import numpy as np
+import os
+from keras.layers import Dense, Flatten,Dropout
+from keras.models import Sequential
+from keras.optimizers import Adam
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from keras.losses import binary_crossentropy
+import h5py
+from keras.callbacks import ModelCheckpoint
+def isGroup(obj):
+    if isinstance(obj, h5py.Group):
+        return True
+    return False
+
+
+def isDataset(obj):
+    if isinstance(obj, h5py.Dataset):
+        return True
+    return False
+
+
+def getdatastefromgroup(dataset, obj):
+    if (isGroup(obj)):
+        for key in obj:
+            x = obj[key]
+            getdatastefromgroup(dataset, x)
+    else:
+        dataset.append(obj)
+
+
+def geWeightforlayer(layername, filename):
+    weight = []
+    with h5py.File(filename, mode='r') as f:
+        for keys in f:
+            if layername in keys:
+                obj = f[keys]
+                datasets = []
+                getdatastefromgroup(datasets, obj)
+            for dataset in datasets:
+                w = np.array(dataset)
+                weight.append(w)
+    return weight
+
+
+def printWeightforlayer(layername,filename):
+    with h5py.File(filename,mode='r') as f:
+        for keys in f:
+            print(keys,f[keys])
+            o=f[keys]
+            for key1 in o:
+                print(key1,o[key1])
+                r=o[key1]
+                for key2 in r:
+                    print(key2,r[key2])
+def read_data(data_dirname_path):
+    Trainx = np.load(os.path.join(data_dirname_path,"trainx.npy"))
+    Trainy = np.load(os.path.join(data_dirname_path,"trainy.npy"))
+    # Vaildx = np.load(os.path.join(data_dirname_path,"Vaildx.npy"))
+    # Vaildy = np.load(os.path.join(data_dirname_path,"Vaildy.npy"))
+    Testx  = np.load(os.path.join(data_dirname_path,"testx.npy"))
+    Testy  = np.load(os.path.join(data_dirname_path,"testy.npy"))
+
+
+    Trainx =Trainx.reshape(-1,106,1)
+    # Vaildx = Vaildx.reshape(-1,166, 586, 1)
+    Testx  = Testx.reshape(-1,106,1)
+
+
+    Trainy  =np_utils.to_categorical(Trainy,num_classes=2)
+    # Vaildy  =np_utils.to_categorical(Vaildy,num_classes=12)
+    Testy   =np_utils.to_categorical(Testy,num_classes=2)
+    return Trainx, Trainy,Testx, Testy
+class RBFLayer(Layer):
+    def __init__(self, units, gamma, **kwargs):
+        super(RBFLayer, self).__init__(**kwargs)
+        self.units = units
+        self.gamma = K.cast_to_floatx(gamma)
+
+    def build(self, input_shape):
+#         print(input_shape)
+#         print(self.units)
+        self.mu = self.add_weight(name='mu',
+                                  shape=(int(input_shape[1]), self.units),
+                                  initializer='uniform',
+                                  trainable=True)
+        super(RBFLayer, self).build(input_shape)
+
+    def call(self, inputs):
+        diff = K.expand_dims(inputs) - self.mu
+        l2 = K.sum(K.pow(diff, 2), axis=1)
+        # res = K.exp(-1 * self.gamma * l2)
+        res = 1/K.sqrt(1 + self.gamma * l2)
+        return res
+
+    def compute_output_shape(self, input_shape):
+        return (input_shape[0], self.units)
+
+
+
+def RBF():
+    model = Sequential()
+    model.add(Dense(106, input_dim=(106)))
+    model.add(RBFLayer(2, 0.5))
+    # model.add(Dropout(0.2))
+    # model.add(Dense(2, activation='softmax'))
+    model.summary()
+    # 保存模型
+    model.save('RBF.h5')
+    return model
+def NN():
+
+    model = Sequential()
+    model.save_weights(checkpoint_path.format(epoch=0))
+    model.add(Dense(2, input_dim=(106), activation='relu'))
+
+    # model.add(Dense(32, activation='relu'))
+    # model.add(Dropout(0.5))
+    # model.add(Dense(8, activation='relu'))
+    # model.add(Dense(64, activation='relu'))
+    # model.add(Dense(2, activation='softmax'))
+    # 查看网络结构
+    model.summary()
+    # 保存模型
+    return model
+if __name__ == "__main__":
+    Trainx, Trainy,Testx, Testy=read_data(os.path.join(os.path.dirname(__file__),'adult_dataset'))
+    print(Trainy.shape)
+    model_name='weight'
+    # model_name='NN'
+    if model_name=='RBF':
+        Trainx = Trainx.reshape(Trainx.shape[0], (106))
+        Testx = Testx.reshape(Testx.shape[0], (106))
+        # print(Trainx.shape)
+        # os.system('pause')
+        model = RBF()
+        model.compile(optimizer=Adam(lr=0.00025, beta_1=0.9, beta_2=0.999, epsilon=1e-08),loss='binary_crossentropy', metrics=['accuracy'])
+        History = model.fit(Trainx, Trainy, batch_size=300, epochs=15, verbose=2,validation_data=(Testx,Testy))
+        pre = model.evaluate(Testx, Testy, batch_size=100, verbose=2)
+        print('test_loss:', pre[0], '- test_acc:', pre[1])
+        plt.figure(figsize=(15, 5))
+        plt.subplot(1, 2, 1)
+        plt.plot(History.history['accuracy'])
+        plt.plot(History.history['val_accuracy'])
+        plt.title('RBF accuracy')
+        plt.ylabel('accuracy')
+        plt.xlabel('epoch')
+        plt.legend(['train', 'test'], loc='upper left')
+        plt.subplot(1, 2, 2)
+        plt.plot(History.history['loss'])
+        plt.plot(History.history['val_loss'])
+        plt.title('RBF loss')
+        plt.ylabel('loss')
+        plt.xlabel('epoch')
+        plt.legend(['train', 'test'], loc='upper left')
+        plt.show()
+        plt.show()
+    if model_name=='NN':
+        checkpoint_path = "adult_model/cp_{epoch:04d}.hdf5"
+
+        checkpoint=ModelCheckpoint(filepath=checkpoint_path,
+                                   verbose=0,
+                                   save_best_only=False,
+                                   save_weights_only=True,
+                                   mode="auto",
+                                   period=1)
+        Trainx = Trainx.reshape(Trainx.shape[0], (106))
+        Testx = Testx.reshape(Testx.shape[0], (106))
+        # print(Trainx.shape)
+        # os.system('pause')
+        model = NN()
+        model.compile(optimizer=Adam(lr=0.0008, beta_1=0.9, beta_2=0.999, epsilon=1e-08),loss='binary_crossentropy', metrics=['accuracy'])
+        History = model.fit(Trainx, Trainy, batch_size=300, epochs=40, verbose=2,validation_data=(Testx,Testy),callbacks=[checkpoint])
+        pre = model.evaluate(Testx, Testy, batch_size=100, verbose=2)
+        print('test_loss:', pre[0], '- test_acc:', pre[1])
+        from tensorflow.python import pywrap_tensorflow
+        plt.figure(figsize=(15, 5))
+        plt.subplot(1, 2, 1)
+        plt.plot(History.history['accuracy'])
+        plt.plot(History.history['val_accuracy'])
+        plt.title('NN accuracy')
+        plt.ylabel('accuracy')
+        plt.xlabel('epoch')
+        plt.legend(['train', 'test'], loc='upper left')
+        plt.subplot(1, 2, 2)
+        plt.plot(History.history['loss'])
+        plt.plot(History.history['val_loss'])
+        plt.title('NN loss')
+        plt.ylabel('loss')
+        plt.xlabel('epoch')
+        plt.legend(['train', 'test'], loc='upper left')
+        plt.show()
+        plt.show()
+    if model_name=='weight':
+
+
+        path=os.path.join(os.path.dirname(__file__),'adult_model')
+
+        allFileList = os.listdir(path)
+        # printWeightforlayer(layername='',filename=os.path.join(path,'cp_0001.hdf5'))
+        epoch_weight=[]
+        epoch_bias=[]
+        for file in allFileList:
+            weight = geWeightforlayer(layername="dense_1", filename=os.path.join(path,file))
+            layerweight=weight[1].flatten()
+            layerbias=weight[0]
+            epoch_weight.append(layerweight)
+            epoch_bias.append(layerbias)
+        epoch_weight=np.array(epoch_weight)
+        epoch_bias=np.array(epoch_bias)
+
+        plt.figure(figsize=(10, 10))
+        # 設定圖的範圍, 不設的話，系統會自行決定
+        plt.xlim(1, 10)
+        plt.ylim(-1,1)
+        # 照需要寫入x 軸和y軸的 label 以及title
+        plt.xlabel("epoch")
+        plt.ylabel("weight_value")
+        plt.title("The Title")
+        x=[i+1 for i in range(epoch_bias.shape[0])]
+        for line_num in range(epoch_bias.shape[1]):
+            single_weight=epoch_bias[:,line_num]
+            print(single_weight)
+            plt.plot(x,single_weight)
+
+            # os.system('pause')
+        plt.show()
+
--- preprocess.py
+++ preprocess.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import gzip
 import os
+from sklearn.preprocessing import MinMaxScaler
+import copy
 import matplotlib.pyplot as plt
 def load_mnist_data(xpath,ypath,num_images):
     f = gzip.open(os.path.join(os.path.dirname(__file__),xpath), 'r')
@@ -11,6 +13,7 @@ def load_mnist_data(xpath,ypath,num_images):
     buf = f.read(image_size * image_size * num_images)
     data = np.frombuffer(buf, dtype=np.uint8).astype(np.float32)
     data = data.reshape(num_images, image_size, image_size, 1)
+    data=data/255
 
     f = gzip.open(os.path.join(os.path.dirname(__file__),ypath), 'r')
     f.read(8)
@@ -35,6 +38,7 @@ def get_mnist_dataset():
     #
     trainx,trainy=load_mnist_data(xpath='test_train/train-images-idx3-ubyte.gz',ypath='test_train/train-labels-idx1-ubyte.gz',num_images=30000)
     testx, testy = load_mnist_data(xpath='test_train/t10k-images-idx3-ubyte.gz',ypath='test_train/t10k-labels-idx1-ubyte.gz',num_images=10000)
+
     np.save('mnist_dataset/trainx.npy', trainx)
     np.save('mnist_dataset/trainy.npy', trainy)
     np.save('mnist_dataset/testx.npy', testx)
@@ -47,10 +51,10 @@ def get_adult_dataset():
     intTestSize = len(dfDataTest)
     # print(dfDataTrain.keys())
     dfDataTrainY = dfDataTrain["income"]
+    # print(type(dfDataTrainY))
     dfTrainY = pd.DataFrame((dfDataTrainY == " >50K").astype("int64"), columns=["income"])  # >50K 1, =<50K 0
-    # print(dfDataTrain)
-    dfDataTestY = dfDataTrain["income"]
-    dfTestY = pd.DataFrame((dfDataTestY == " >50K").astype("int64"), columns=["income"])  # >50K 1, =<50K 0
+    dfDataTestY = dfDataTest["income"]
+    dfTestY = pd.DataFrame((dfDataTestY == " >50K.").astype("int64"), columns=["income"])  # >50K 1, =<50K 0
     dfDataTrain = dfDataTrain.drop(["income"], axis=1)
     dfDataTest = dfDataTest.drop(["income"], axis=1)
 
@@ -82,14 +86,25 @@ def makeDataProcessing(dfData):
     listObjectColumnName = [col for col in dfDataX.columns if dfDataX[col].dtypes=="object"]
     listNonObjectColumnName = [col for col in dfDataX.columns if dfDataX[col].dtypes!="object"]
 
+
     dfNonObjectData = dfDataX[listNonObjectColumnName]
-    dfNonObjectData.insert(2, "sex", (dfData["sex"]==" Male").astype(np.int)) # Male 1 Femal 0
+    print(dfNonObjectData)
+    normalize_dict={}
+    for index,keys in enumerate(dfNonObjectData.keys()):
+        print(keys)
+        print(type(dfNonObjectData))
+        deal_arrary=dfNonObjectData[keys].values.reshape(-1,1)
+        normalize_data= np.squeeze(MinMaxScaler().fit_transform(deal_arrary))
+        normalize_dict[str(keys)]=normalize_data
+    dfdfNonObjectData_Normallize = pd.DataFrame(data=normalize_dict)
+    dfdfNonObjectData_Normallize.insert(2, "sex", (dfData["sex"]==" Male").astype(np.int)) # Male 1 Femal 0
+
 
     dfObjectData = dfDataX[listObjectColumnName]
     dfObjectData = pd.get_dummies(dfObjectData)
 
-    dfDataX = dfNonObjectData.join(dfObjectData)
-    dfDataX = dfDataX.astype("int64")
+    dfDataX = dfdfNonObjectData_Normallize.join(dfObjectData)
+    dfDataX = dfDataX.astype("float")
     return dfDataX
 def get_bike_dataset():
     dfData= pd.read_csv(os.path.join(os.path.dirname(__file__),'test_train/day.csv'))
@@ -113,8 +128,12 @@ def get_bike_dataset():
     dfTestY.to_csv(os.path.join(os.path.dirname(__file__), "bike_dataset/Y_Test_my.csv"), index=False)
     arrayTrainX = np.array(dfTrainX.values)
     arrayTestX = np.array(dfTestX.values)
-    arrayTrainY = np.array(dfTrainY.values)
-    arrayTestY= np.array(dfTestY.values)
+    arrayTrainY =np.array(dfTrainY.values.astype(int)).reshape(-1,1)
+    arrayTestY= np.array(dfTestY.values.astype(int)).reshape(-1,1)
+    arrayTestY=MinMaxScaler().fit_transform(arrayTestY)
+    arrayTrainY=MinMaxScaler().fit_transform(arrayTrainY)
+    # print(arrayTrainY)
+    # arrayTrainY=sklearn.preprocessing.maxabs_scale(arrayTrainY,axis=0,copy=True)
     np.save(os.path.join(os.path.dirname(__file__), "bike_dataset/trainx.npy"),arrayTrainX)
     np.save(os.path.join(os.path.dirname(__file__), "bike_dataset/trainy.npy"), arrayTrainY)
     np.save(os.path.join(os.path.dirname(__file__), "bike_dataset/testx.npy"),arrayTestX)
@@ -124,4 +143,6 @@ if __name__ == "__main__":
     pass
 get_adult_dataset()
 # get_mnist_dataset()
-get_bike_dataset()
+# get_bike_dataset()
+# train=np.load('adult_dataset/testy.npy')
+# print(np.max(train))
